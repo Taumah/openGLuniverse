8c470f639abcbdc3299276a61365fbadc5b9581d
Thib93 <gama.thibaut@gmail.com>
2020-04-05 16:06:52 +0200

    Main avant modif

--- src/hand.cpp
+++ src/hand.cpp
@@ -3,20 +3,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-
-
-
+    /*--------- Global ---------------*/		//A declarer en global ou tout mettre dans la meme fonction
+GLUquadricObj *pObj;		//definition du pointeur d'objet
 
 
 extern float angle , angle2;
 
-void hand(float armAngle){
+void rightHand(float handAngle){
 
 
-
-    /*--------- Global ---------------*/		//A declarer en global ou tout mettre dans la meme fonction
-GLUquadricObj *pObj;		//definition du pointeur d'objet
 /*--------- INIT ---------------*/		//En phase d'initialisation
 pObj = gluNewQuadric();		//creation d'un objet(Retourne 0 si No Memory)
 
@@ -25,8 +20,13 @@ gluDeleteQuadric (pObj);		//detruit l'objet
 
 
 
-
     glPushMatrix();
+        glTranslatef(-2, 0, 0);
         gluCylinder(pObj, 20, 20, 20, 20, 20);
+        glRotatef(handAngle, 1, 0, 1);
+        glTranslatef(-2, 0, 0);
+        //Avant Bras
+
+
     glPopMatrix();
 }

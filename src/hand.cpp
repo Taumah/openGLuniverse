66cdc56ef8c39348df4ded9d097bccb46632b6bd
Thib93 <gama.thibaut@gmail.com>
2020-04-24 15:39:05 +0200

    main

--- src/hand.cpp
+++ src/hand.cpp
@@ -13,17 +13,34 @@
 
 void hand(float armAngle){
 
+    int inclinaison = 50;
     
 
+    for(int i = 0; i < 5; i++){       
+        glPushMatrix();
+                glRotatef(100, inclinaison, 20, 20);
+                glTranslatef(0, 0, 0.5);
+                GLUquadricObj *very_bottom_head;
+                very_bottom_head = gluNewQuadric();
+                gluQuadricNormals(very_bottom_head, GLU_SMOOTH);
+                glColor3f(.93, .85, .02);
+                gluCylinder(very_bottom_head, 0.15, 0.15, 1, 1024, 1024);
+                gluDeleteQuadric(very_bottom_head);
+            glPopMatrix();
+        inclinaison -= 10;        
+    }
+
     glPushMatrix();
-            glRotatef(-90, 1, 1, 2);
-            GLUquadricObj *very_bottom_head;
-            very_bottom_head = gluNewQuadric();
-            gluQuadricNormals(very_bottom_head, GLU_SMOOTH);
-            glColor3f(0.8, 0.8 , 0.7);
-            gluCylinder(very_bottom_head, 0.50, 0.50, 1, 1024, 1024);
-            gluDeleteQuadric(very_bottom_head);
-        glPopMatrix();
+                glRotatef(50, 50, 50, 20);
+                glScalef(0.5, 1, 1);
+                GLUquadricObj *very_bottom_head;
+                very_bottom_head = gluNewQuadric();
+                gluQuadricNormals(very_bottom_head, GLU_SMOOTH);
+                glColor3f(.93, .85, .02);
+                gluCylinder(very_bottom_head, 0.5, 0.5, 1, 1024, 1024);
+                gluDeleteQuadric(very_bottom_head);
+            glPopMatrix();
+
 
     
 }

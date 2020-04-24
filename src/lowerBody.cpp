8916fa96e072af329e4fd5f07b4539e2303406b3
antoineThiel <antoine.thiel@hotmail.fr>
2020-04-24 14:38:36 +0200

    closed bottom part body

--- src/lowerBody.cpp
+++ src/lowerBody.cpp
@@ -16,5 +16,14 @@ void lowerBody(){
             gluCylinder(low_lowpart, 2, .8, 2.5, 1024, 1024);
             gluDeleteQuadric(low_lowpart);
         glPopMatrix();
+
+        glTranslatef(0, -2.3, 0);
+        glPushMatrix();
+                GLUquadricObj *bottom_sphere;
+                bottom_sphere = gluNewQuadric();
+                gluQuadricNormals(bottom_sphere, GLU_SMOOTH);
+                gluSphere(bottom_sphere, .8, 1024, 1024);
+                gluDeleteQuadric(bottom_sphere);
+            glPopMatrix();
     glPopMatrix();
 }
